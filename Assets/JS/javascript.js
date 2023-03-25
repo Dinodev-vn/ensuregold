2a1b0ba1f15476de6060c1cb3d01bfcf42932865
Dinodev-vn <duyharuno@gmail.com>
2023-03-25 20:21:39 +0700

    upload source code

--- Assets/JS/javascript.js
+++ Assets/JS/javascript.js
@@ -30,15 +30,23 @@ document.getElementById('prev').onclick = function() {
 // setInterval(slide, 3000);
 
 
-if (window.innerWidth < 376) {
+if (window.innerWidth < 476) {
     // slider product
 const slider = document.querySelector('#formList');
 const sliderContainer = slider.querySelector('#list');
-const productItem = slider.querySelectorAll('.item');
+let productItem = slider.querySelectorAll('.item');
 
+let widthFormList = slider.offsetWidth;
+for (let i = 0; i < productItem.length; i++) {
+  productItem[i].style.width = widthFormList + "px";
+}
 let currentIndex = 0;
 let itemWidth = productItem[0].clientWidth;
 let containerWidth = itemWidth * productItem.length;
+let fullWidth = containerWidth + "px";
+
+sliderContainer.style.setProperty('--before-width', fullWidth);
+console.log("full width: " + fullWidth);
 
 sliderContainer.style.width = containerWidth + 'px';
 
@@ -50,5 +58,23 @@ function slide() {
   sliderContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
 }
 
+function setWidth() {
+  productItem = slider.querySelectorAll('.item');
+
+  widthFormList = slider.offsetWidth;
+  for (let i = 0; i < productItem.length; i++) {
+    productItem[i].style.width = widthFormList + "px";
+  }
+
+   itemWidth = widthFormList;
+   containerWidth = itemWidth * productItem.length;
+   fullWidth = containerWidth + "px";
+
+  sliderContainer.style.setProperty('--before-width', fullWidth);
+
+  sliderContainer.style.width = containerWidth + 'px';
+}
+
+setInterval(setWidth, 1000);
 setInterval(slide, 3000);
 }
